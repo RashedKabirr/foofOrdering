8d5eed9b5ebf84bc8855712eca6da3e81274456f
Ahmad Rashed Kabir <kabirrashed72@gmail.com>
2024-07-15 00:35:48 +0200

    fist UI added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,45 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import Colors from '@/constants/Colors';
+import products from '@/assets/data/products';
 
+
+
+const product = products[0];
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Rashed is rich</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{uri: product.image}} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0d1c1',
+    padding: 10,
+    borderRadius: 30
+
+   
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: '600',
+    color: Colors.light.text
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+ 
+  price : {
+    color: Colors.light.tint,
+    fontWeight: 'bold'
   },
+  image : {
+    width: '100%',
+    aspectRatio: 1
+  }
 });
