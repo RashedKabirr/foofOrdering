5856e6b34441f14f6f4a70218da70970fa09a7c9
Ahmad Rashed Kabir <kabirrashed72@gmail.com>
2024-07-16 00:15:29 +0200

    passing name according to id

--- app/(tabs)/menu/[id].tsx
+++ app/(tabs)/menu/[id].tsx
@@ -5,13 +5,17 @@ import { Text, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 import ProductListItem from '@/components/ProductListItem';
 import products from '@/assets/data/products';
-import { useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams } from 'expo-router';
 
-const id = useLocalSearchParams();
+
 const ProductDetailScreen = () =>{
+   const {id} = useLocalSearchParams();
+   const product = products.find((p) => p.id.toString() === id);
+
    return(
     <View>
-    <Text>this is products is id :  </Text>
+      <Stack.Screen options={{title : product?.name }} />
+    <Text>this is products is id : {id}</Text>
 </View>
    );
 };
